f38f708312232c2c174058106e746012bf03bbad
QuiK000 <teamquiksteam@gmail.com>
2025-10-25 17:56:49 +0300

    Implement brute force protection mechanism in Auth service with Redis-based IP blocking, attempt tracking, and cleanup logic

---
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/security/BruteForceProtectionFilter.java
@@ -0,0 +1,27 @@
+package com.dev.quikkkk.auth_service.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BruteForceProtectionFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
