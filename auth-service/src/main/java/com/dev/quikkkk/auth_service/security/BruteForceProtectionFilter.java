ede8a31c812057816ee1e44f503b549c8cb41758
QuiK000 <teamquiksteam@gmail.com>
2025-10-25 18:00:06 +0300

    Integrate brute force protection filter with IP blocking for login and registration endpoints.

--- auth-service/src/main/java/com/dev/quikkkk/auth_service/security/BruteForceProtectionFilter.java
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/security/BruteForceProtectionFilter.java
@@ -1,5 +1,6 @@
 package com.dev.quikkkk.auth_service.security;
 
+import com.dev.quikkkk.auth_service.service.IBruteForceProtectionService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,16 +13,46 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static jakarta.servlet.http.HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class BruteForceProtectionFilter extends OncePerRequestFilter {
+    private final IBruteForceProtectionService service;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        String requestUri = request.getRequestURI();
+        String clientIp = getClientIp(request);
 
+        log.info("Request URI: {}, Client IP: {}", requestUri, clientIp);
+
+        if (requestUri.equals("/api/v1/auth/login") || requestUri.equals("/api/v1/auth/register")) {
+            if (service.isBlocked(clientIp)) {
+                log.warn("Blocked request from IP: {} to {}", clientIp, requestUri);
+
+                response.setStatus(SC_REQUEST_ENTITY_TOO_LARGE);
+                response.getWriter().write("Too many requests. Please try again later.");
+
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) ip = request.getHeader("Proxy-Client-IP");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) ip = request.getHeader("WL-Proxy-Client-IP");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) ip = request.getRemoteAddr();
+
+        return ip;
     }
 }
