5c5a4b85de95f01b562f3f8c88ca155cfc106920
QuiK000 <teamquiksteam@gmail.com>
2025-10-25 17:45:08 +0300

    Define custom exception handling framework in Auth service with `ErrorCode`, `BusinessException`, and placeholder `AuthenticationExceptionHandler`.

---
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/exception/AuthenticationExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.dev.quikkkk.auth_service.exception;
+
+public class AuthenticationExceptionHandler {
+}
---
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/exception/ErrorCode.java
@@ -0,0 +1,38 @@
+package com.dev.quikkkk.auth_service.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+public enum ErrorCode {
+    USER_NOT_FOUND("USER_NOT_FOUND", "User not found with id %s", HttpStatus.NOT_FOUND),
+    INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR),
+    ENTITY_NOT_FOUND("ENTITY_NOT_FOUND", "Entity not found with id %s", HttpStatus.NOT_FOUND),
+    BAD_REQUEST("BAD_REQUEST", "Bad request", HttpStatus.BAD_REQUEST),
+    FORBIDDEN("FORBIDDEN", "Forbidden", HttpStatus.FORBIDDEN),
+    UNAUTHORIZED("UNAUTHORIZED", "Unauthorized", HttpStatus.UNAUTHORIZED),
+    BAD_CREDENTIALS("BAD_CREDENTIALS", "Bad credentials", HttpStatus.UNAUTHORIZED),
+    USERNAME_NOT_FOUND("USERNAME_NOT_FOUND", "Username not found", HttpStatus.UNAUTHORIZED),
+    EMAIL_ALREADY_EXISTS("EMAIL_ALREADY_EXISTS", "Email already exists", HttpStatus.BAD_REQUEST),
+    USERNAME_ALREADY_EXISTS("USERNAME_ALREADY_EXISTS", "Username already exists", HttpStatus.BAD_REQUEST),
+    PASSWORD_MISMATCH("PASSWORD_MISMATCH", "Password mismatch", HttpStatus.BAD_REQUEST),
+    TOO_MANY_ATTEMPTS("TOO_MANY_ATTEMPTS", "Too many attempts", HttpStatus.UNAUTHORIZED),
+    INVALID_CREDENTIALS("INVALID_CREDENTIALS", "Invalid credentials", HttpStatus.UNAUTHORIZED),
+    ROLE_NOT_FOUND("ROLE_NOT_FOUND", "Role not found", HttpStatus.BAD_REQUEST),
+    TOO_MANY_EMAIL_ATTEMPTS("TOO_MANY_EMAIL_ATTEMPTS", "Too many email attempts" , HttpStatus.TOO_MANY_REQUESTS),
+    INVALID_VERIFICATION_CODE("INVALID_VERIFICATION_CODE", "Invalid verification code", HttpStatus.BAD_REQUEST),
+    VERIFICATION_CODE_EXPIRED("VERIFICATION_CODE_EXPIRED", "Verification code expired", HttpStatus.BAD_REQUEST),
+    MAX_VERIFICATION_ATTEMPTS_EXCEEDED("MAX_VERIFICATION_ATTEMPTS_EXCEEDED", "Maximum verification attempts exceeded", HttpStatus.TOO_MANY_REQUESTS),
+    EMAIL_ALREADY_VERIFIED("EMAIL_ALREADY_VERIFIED", "Email already verified", HttpStatus.BAD_REQUEST);
+
+    private final String code;
+    private final String defaultMessage;
+    private final HttpStatus status;
+
+    ErrorCode(String code, String defaultMessage, HttpStatus status) {
+        this.code = code;
+        this.defaultMessage = defaultMessage;
+        this.status = status;
+    }
+}
