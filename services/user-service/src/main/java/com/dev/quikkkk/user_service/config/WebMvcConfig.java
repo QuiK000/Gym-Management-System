17f5a3ec270b974241ebd2ce007dac209078c530
QuiK000 <teamquiksteam@gmail.com>
2025-10-31 12:41:40 +0200

    Merge `WebConfig` into `WebMvcConfig` to consolidate configuration
    - Move `PagedResourcesAssembler` bean registration from `WebConfig` to `WebMvcConfig`.
    - Add `HateoasPageableHandlerMethodArgumentResolver` bean and register it as an argument resolver for pageable requests.

--- services/user-service/src/main/java/com/dev/quikkkk/user_service/config/WebMvcConfig.java
+++ services/user-service/src/main/java/com/dev/quikkkk/user_service/config/WebMvcConfig.java
@@ -1,12 +1,18 @@
 package com.dev.quikkkk.user_service.config;
 
+import com.dev.quikkkk.user_service.dto.response.UserProfileResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -19,5 +25,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
         String uploadPathString = uploadPath.toUri().toString();
 
         registry.addResourceHandler("/uploads/avatars/**").addResourceLocations(uploadPathString);
+    }
+
+    @Bean
+    public HateoasPageableHandlerMethodArgumentResolver pageableResolver() {
+        return new HateoasPageableHandlerMethodArgumentResolver();
+    }
+
+    @Bean
+    public PagedResourcesAssembler<UserProfileResponse> pagedResourcesAssembler() {
+        return new PagedResourcesAssembler<>(pageableResolver(), null);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(pageableResolver());
     }
 }
